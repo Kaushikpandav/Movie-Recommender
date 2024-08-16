34ef2da2abcfe600a7b62a33e93ffba4e82a65c6
kaushik pandav <91875698+Kaushikpandav@users.noreply.github.com>
2024-08-16 17:43:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import requests
 
 
 def fetch_poster(movie_id):
-    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key={EnterYOURkey}&language=en-US'.format(movie_id)) 
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=24e1513fe817f5c6bd8bb51cfd6092e2&language=en-US'.format(movie_id)) 
     
     data = response.json()
     return " https://image.tmdb.org/t/p/w500/" + data['poster_path']
